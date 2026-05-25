2ff438d63b9b9ff3f31a907e79c6bd4b7191257b
lwh <lwh01895079@alibaba-inc.com>
2026-05-25 17:54:28 +0800

    优化环境参数配置

--- config/llm_config.py
+++ config/llm_config.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
 
-api_key= 'sk-24acd92d862f4651a768ec270b630c99'
-base_url = "https://api.deepseek.com"
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+base_url = os.getenv("BASE_URL", "https://api.deepseek.com")
+model = os.getenv("MODEL", "deepseek-v4-pro")
